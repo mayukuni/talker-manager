fede1474c970946c89b1cd4db62afa6e622d5f6c
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-06-22 18:34:48 -0300

    teste requisito 5

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,12 +1,13 @@
 const token = require('./token');
 
 const authorizationFunc = (req, res, next) => {
+  const test = token();
   const { authorization } = req.headers;
 
   if (!authorization) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
-  if (authorization !== token) {
+  if (authorization !== test) {
     return res.status(401).json({ message: 'Token inválido' });
   }
 
