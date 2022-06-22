19d7c4bebe4681026949213df3eebfff6969b47d
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-06-22 13:37:40 -0300

    teste requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const talkersJson = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +17,11 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+// requisito 1
+app.get('/talker', async (_req, res) => {
+  const talkerFile = await fs.readFile(talkersJson, 'utf-8');
+  const talkers = await JSON.parse(talkerFile);
+  res.status(200).json(talkers);
 });
